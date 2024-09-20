8f10ac0239a36a4df80ce4d0c557e84a80b72d24
X1STY <x1stygg@gmail.com>
2024-09-20 11:34:39 +0700

    add tests for alphabetic translations

--- inf_sec_test/AlphabeticOperationsUnitTest.cs
+++ inf_sec_test/AlphabeticOperationsUnitTest.cs
@@ -43,5 +43,18 @@ namespace inf_sec_test
             var result = alphabet.getDiff(char1, char2);
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void TextToArrayToText()
+        {
+            string alph = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ_";
+            uint[] arr = new uint[alph.Length];
+            arr = [1, 2, 3, 4 ,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ,21 ,22 ,23 ,24 ,25 ,26 ,27 ,28 ,29, 30, 31, 0];
+            var alphabet = new AlphabetOperations();
+            var result = alphabet.textToArray(alph);
+            Assert.Equal(arr, result);
+            var result1 = alphabet.arrayToText(arr);
+            Assert.Equal(alph, result1);
+        }
     }
 }
